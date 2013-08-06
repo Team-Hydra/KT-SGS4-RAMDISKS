7fc21afe88acb826058a135947c6206658230235
Matt Filetto <matt.filetto@gmail.com>
2013-08-06 15:25:56 -0700

    TW-JB4.3: Update for ChronicKernel
    Change-Id: I39d73728472d0f957f07e99931f3f3cf7051d848

--- TW_INTL-JB4.3/sbin/ext/post-init.sh
+++ TW_INTL-JB4.3/sbin/ext/post-init.sh
@@ -1,66 +1,52 @@
 #!/sbin/busybox sh
 #
-#Set Max Mhz for GPU
-echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
-
-#Set Max Mhz speed and booted flag to set Super Max
-echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_booted;
 
 # Remount FileSys RW
 /sbin/busybox mount -t rootfs -o remount,rw rootfs
 
 ## Create the kernel data directory
-if [ ! -d /data/.ktoonsez ];
+if [ ! -d /data/chronic ];
 then
-  mkdir /data/.ktoonsez
-  chmod 777 /data/.ktoonsez
+  mkdir /data/chronic
+  chmod 777 /data/chronic
 fi
 
 ## Enable "post-init" ...
-if [ -f /data/.ktoonsez/post-init.log ];
+if [ -f /data/chronic/post-init.log ];
 then
   # BackUp old post-init log
-  mv /data/.ktoonsez/post-init.log /data/.ktoonsez/post-init.log.BAK
+  mv /data/chronic/post-init.log /data/chronic/post-init.log.BAK
 fi
 
 # Start logging
-date >/data/.ktoonsez/post-init.log
-exec >>/data/.ktoonsez/post-init.log 2>&1
+date >/data/chronic/post-init.log
+exec >>/data/chronic/post-init.log 2>&1
 
 echo "Running Post-Init Script"
 
-#Check for forcing Samsung MTP
-if [ -f /data/.ktoonsez/force_samsung_mtp ];
-then
-  # BackUp old post-init log
-  echo "Forcing Samsung MTP"
-  echo 1 > /sys/devices/system/cpu/cpufreq/ktoonsez/enable_google_mtp;
-fi
-
 ## Testing: Check for ExFat SD Card
 #
-SDTYPE1=`blkid /dev/block/mmcblk1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
-SDTYPE2=`blkid /dev/block/mmcblk1p1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
+SDTYPE=`blkid /dev/block/mmcblk1p1  | awk '{ print $3 }' | sed -e 's|TYPE=||g' -e 's|\"||g'`
 
-if [ "${SDTYPE1}" == "exfat" ];
-then
-  echo "ExFat-Debug: SD-Card is type ExFAT"
-  echo "ExFat-Debug: trying to mount via fuse"
-  mount.exfat-fuse /dev/block/mmcblk1 /storage/extSdCard
-else
-  echo "ExFat-Debug: SD-Card is type1: ${SDTYPE1}"
-fi
-if [ "${SDTYPE2}" == "exfat" ];
+if [ ${SDTYPE} == "exfat" ];
 then
   echo "ExFat-Debug: SD-Card is type ExFAT"
   echo "ExFat-Debug: trying to mount via fuse"
   mount.exfat-fuse /dev/block/mmcblk1p1 /storage/extSdCard
 else
-  echo "ExFat-Debug: SD-Card is type2: ${SDTYPE2}"
+  echo "ExFat-Debug: SD-Card is type: ${SDTYPE}"
 fi
 
 # Remount FileSys RO
 /sbin/busybox mount -t rootfs -o remount,ro rootfs
+
+#Set Max Mhz for GPU
+echo 450000000 > /sys/devices/platform/kgsl-3d0.0/kgsl/kgsl-3d0/max_gpuclk
+
+#Set governor items
+echo 378000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
+echo 1890000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_booted;
+echo 1 > /sys/devices/system/cpu/cpufreq/ktoonsez/enable_oc;
 
 echo $(date) END of post-init.sh
