2aac61e1cec9ba5033535d68af5754ed90feec99
Matt Filetto <matt.filetto@gmail.com>
2014-07-08 23:59:57 -0700

    G900P: TW: fix max_gpuclk sysfs path and set min gpu freq to 320mhtz
    Change-Id: I8bf441eaa8bd5b7bc34dea836b31744452dd9066

--- G900P/TW_KK4.4/sbin/post-init.sh
+++ G900P/TW_KK4.4/sbin/post-init.sh
@@ -1,5 +1,8 @@
 #!/system/bin/sh
 
+echo 578000000 > /sys/class/kgsl/kgsl-3d0/max_gpuclk;
+echo 4 > /sys/class/kgsl/kgsl-3d0/min_pwrlevel;
+
 #Set Min/Max Mhz speed and booted flag to set Super Max
 echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
 echo 2457600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
