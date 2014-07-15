8dbbdc88b350a4866038a2e86ce0debb1d190005
Matt Filetto <matt.filetto@gmail.com>
2014-07-14 19:58:26 -0700

    G900P: AOSP: add custom post-init.sh
    Change-Id: I547f2f0f0dc8ccc542a7f91c28191044e31a3973

---
+++ G900P/AOSP/sbin/post-init.sh
@@ -0,0 +1,20 @@
+#!/system/bin/sh
+
+echo 578000000 > /sys/class/kgsl/kgsl-3d0/max_gpuclk;
+echo 4 > /sys/class/kgsl/kgsl-3d0/min_pwrlevel;
+
+#Set Min/Max Mhz speed and booted flag to set Super Max
+echo 300000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq;
+echo 2457600 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq;
+echo 300000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq;
+echo 2457600 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq;
+echo 300000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq;
+echo 2457600 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq;
+echo 300000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq;
+echo 2457600 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq;
+
+#enable oc
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/enable_oc;
+echo 1 > /sys/devices/system/cpu/cpu1/cpufreq/enable_oc;
+echo 1 > /sys/devices/system/cpu/cpu2/cpufreq/enable_oc;
+echo 1 > /sys/devices/system/cpu/cpu3/cpufreq/enable_oc;
